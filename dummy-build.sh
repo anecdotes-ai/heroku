0b44b6d5f7ac117ad46b82db57d8961056cdaacb
Ido Ozeri <ido@anecdotes.ai>
2021-09-22 11:28:38 +0300

    Adding heroku.yml and dummy-build.sh

---
+++ dummy-build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2021 anecdotes.ai;
+# Author:     Ido Ozeri <ido@anecdotes.ai>
+# Maintainer: Ido Ozeri <ido@anecdotes.ai>
+# Purpose: dummy-build for Heroku
+
+
+# Variables
+
+DIR=$(dirname $0)
+PROG=$(basename $0)
+
+
+
+########################## MAIN WORKFLOW ###########################
+
+echo "[$PROG][INFO]: Starting Build"
+echo "[$PROG][INFO]: Build Completed"
+
+exit 0
